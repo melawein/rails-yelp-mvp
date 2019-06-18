b559b20d2c63762dadaab5eb11ab3929e6146d50
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-06-18 14:11:12 +0300

    Final Yelp App

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,27 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant, only: [:new, :create]
   def index
-    @reviews = Review.all
-  end
-
-  def create
+    @review = Review.find(params[:id])
   end
 
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
